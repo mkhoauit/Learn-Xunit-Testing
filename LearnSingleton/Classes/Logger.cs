c5c616e6f7da7a199368df0c2b209330d2b45ea8
mkhoauit <92783675+mkhoauit@users.noreply.github.com>
2021-12-02 17:09:49 +0700

    Fix Singleton

---
+++ LearnSingleton/Classes/Logger.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnSingleton.Classes
+{
+    public sealed class Logger
+    {
+        private static Logger _logger;
+        public static int instanceCounter = 0;
+        public string Messages="Message:\n";
+        
+        private Logger()
+        {
+            instanceCounter++;  
+            Console.WriteLine($"Instances created:{instanceCounter} time");
+            
+        }
+
+        public static Logger GetLogger()
+        {
+            if (_logger == null)
+            {
+                _logger = new Logger();
+            }
+
+            return _logger;
+        }
+        public string WriteMessage(string message,string name)
+        {
+            if (message ==""||message ==" "||name ==""||name ==" ")
+            {
+                throw new Exception("Please input your message correctly!");
+            }
+            this.Messages += "Message from " +name +": " +message +"\n";
+            return this.Messages;
+        }
+
+        public string FindMessage(string message)
+        {
+            if (Messages.Contains(message))
+            {
+                return ($"There is message: {message}");
+            }
+            else
+            {
+                throw new Exception("Not Found!");
+            }
+            
+        }
+
+    }
+}
---
+++ LearnSingleton/Program.cs
@@ -0,0 +1,52 @@
+﻿using LearnSingleton.Classes;
+using System;
+using System.Security.Cryptography;
+
+namespace LearnSingleton
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many LogMessage do you want?");
+            int log = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < log; i++)
+            {
+                Console.WriteLine($"--Log Message {i+1}:");
+                
+                Console.WriteLine("What's your name?");
+                string name = Convert.ToString(Console.ReadLine());
+                Console.WriteLine("What's your message?");
+                string mess = Convert.ToString(Console.ReadLine());
+                
+                string message = Logger.GetLogger().WriteMessage(mess,name);
+                Console.WriteLine(message);
+                
+            }
+            
+            Console.WriteLine("What message do you want to find?");
+            string find = Convert.ToString(Console.ReadLine());
+           
+
+            Console.WriteLine(Logger.GetLogger().FindMessage(find));
+            
+            if(Logger.instanceCounter==1)
+            {
+                Console.WriteLine("Use 1 instance");
+            }
+            else
+            {
+                Console.WriteLine("Use more than 1");
+                
+            }
+            
+            
+            
+            
+            
+            
+        }
+    }
+}
